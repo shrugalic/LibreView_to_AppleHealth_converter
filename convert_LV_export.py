67e9cbbbde1b832dd29b2eca5bf957197683d360
Oliver Büechi <oliver.bueechi@ergon.ch>
2019-04-25 15:09:10 +0200

    Use 24h time format

--- convert_LV_export.py
+++ convert_LV_export.py
@@ -48,7 +48,6 @@ else: # Local storage
 
 INPUT_FILE_NAME = BASE_DIR + '/LV_' + LIBRE_VIEW_USER + '_Export_' + datetime.date.today().strftime("%m-%d-%Y") + '.csv'
 OUTPUT_FILE_NAME = BASE_DIR + '/' + datetime.date.today().isoformat() + "_blood_glucose.csv"
-
 codecs.register_error("strict", codecs.ignore_errors)
 with codecs.open(INPUT_FILE_NAME, 'rU', 'utf-16-le') as f_in, open(OUTPUT_FILE_NAME, 'w') as f_out:
 	input_reader = csv.reader(f_in, delimiter=',')
@@ -60,7 +59,9 @@ with codecs.open(INPUT_FILE_NAME, 'rU', 'utf-16-le') as f_in, open(OUTPUT_FILE_N
 	writer.writerow(['Start', 'Blood Glucose (mmol<180.1558800000541>/L)'])
 	for row in input_reader:
 		timestamp = row[2]
-		iso_timestamp = datetime.datetime.strptime(timestamp, '%m-%d-%Y %I:%M %p').isoformat()
+		# For 12h format, use this
+		# iso_timestamp = datetime.datetime.strptime(timestamp, '%m-%d-%Y %I:%M %p').isoformat()
+		iso_timestamp = datetime.datetime.strptime(timestamp, '%m-%d-%Y %H:%M').isoformat()
 		measurement = row[4] if row[4] else row[5]
 		if measurement:
 			writer.writerow([iso_timestamp, measurement])
