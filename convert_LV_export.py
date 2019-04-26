99a8b973fc26823c1a42868b4cbb08ed4c5030b8
Oliver Büechi <oliver.bueechi@ergon.ch>
2019-04-26 10:02:45 +0200

    Using anything other than iCloud drive on iOS is a PITA, that's why it's the default on iOS. Cloud deniers can do it on a Mac instead, which was somewhat simplified

--- convert_LV_export.py
+++ convert_LV_export.py
@@ -9,47 +9,37 @@ import platform
 # Your LibreView user name
 LIBRE_VIEW_USER = 'FirstLast'
 
-# Use iCloud drive by default. Set to False to use local storage (next to this script) instead
-USE_ICLOUD_DRIVE = True
-
-# Home directory. /Users/your_short_name on a Mac, something in /var/mobile/… in iOS
-HOME = str(Path.home())
-
-if platform.system() != 'Darwin':
-	print('Only iOS and macOS are supported. Exiting.')
-	import sys
-	sys.exit(1)
-
+# The iCloud (base) directory depends on the platform
+ICLOUD_DRIVE_BASE_PATH = '/Library/Mobile Documents'
 IS_IOS = platform.machine().startswith("iPhone")
+if IS_IOS:
+	ICLOUD_DRIVE_DIR = '/private/var/mobile' + ICLOUD_DRIVE_BASE_PATH
+else: # Presumably a Mac
+	ICLOUD_DRIVE_DIR = str(Path.home()) + ICLOUD_DRIVE_BASE_PATH
 
-if USE_ICLOUD_DRIVE:
-	
-	# Set iCloud drive directory
-	if IS_IOS:
-		FS_ROOT_DIR = '/private/var/mobile'
-	else: # Mac
-		FS_ROOT_DIR = HOME
-	
-	# Local copy of iCloud drive
-	ICLOUD_DRIVE_DIR = FS_ROOT_DIR + '/Library/Mobile Documents'
-	
-	# I'm using the 'Pythonista 3' folder in my iCloud drive as base directory:
-	BASE_DIR = ICLOUD_DRIVE_DIR + '/iCloud~com~omz-software~Pythonista3/Documents'
+# I'm using the 'Pythonista 3' folder in my iCloud drive as base directory:
+ICLOUD_DIR = ICLOUD_DRIVE_DIR + '/iCloud~com~omz-software~Pythonista3/Documents'
 
-else: # Local storage
-	
-	if IS_IOS:
-		# Use the home directory of the app that runs this script (such as Pythonista)
-		BASE_DIR = HOME
-	else: # Mac
-		# Assume the CSV to import is located next to this script
-		import os
-		BASE_DIR = os.path.dirname(__file__) # Directory this script is in
+# Input file: assume the file was exported from LibreView today
+INPUT_FILE_NAME = 'LV_' + LIBRE_VIEW_USER + '_Export_' + datetime.date.today().strftime("%m-%d-%Y") + '.csv'
 
-INPUT_FILE_NAME = BASE_DIR + '/LV_' + LIBRE_VIEW_USER + '_Export_' + datetime.date.today().strftime("%m-%d-%Y") + '.csv'
-OUTPUT_FILE_NAME = BASE_DIR + '/' + datetime.date.today().isoformat() + "_blood_glucose.csv"
+
+# Input file: iCloud drive on iOS, Downloads folder on macOS
+if IS_IOS:
+	INPUT_DIR = ICLOUD_DIR
+else: # Presumably a Mac
+	INPUT_DIR = str(Path.home()) + '/Downloads'
+INPUT_FILE_PATH = INPUT_DIR + '/' + INPUT_FILE_NAME
+
+# Output file: always iCloud. The condition is here for cloud deniers ;)
+OUTPUT_FILE_NAME = datetime.date.today().isoformat() + "_blood_glucose.csv"
+if IS_IOS:
+	OUTPUT_FILE_PATH = ICLOUD_DIR + '/' + OUTPUT_FILE_NAME
+else: # Presumably a Mac
+	OUTPUT_FILE_PATH = ICLOUD_DIR + '/' + OUTPUT_FILE_NAME
+
 codecs.register_error("strict", codecs.ignore_errors)
-with codecs.open(INPUT_FILE_NAME, 'rU', 'utf-16-le') as f_in, open(OUTPUT_FILE_NAME, 'w') as f_out:
+with codecs.open(INPUT_FILE_PATH, 'rU', 'utf-16-le') as f_in, open(OUTPUT_FILE_PATH, 'w') as f_out:
 	input_reader = csv.reader(f_in, delimiter=',')
 	writer = csv.writer(f_out, delimiter=',')
 	next(input_reader)
